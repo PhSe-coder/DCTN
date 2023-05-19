ee45727608c7708a238e09a1cc8d92a3a3af0edf
PhSe-coder <2368732089@qq.com>
2023-05-19 21:08:26 +0800

    scripts

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,4 +5,10 @@ conda install -y pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-
 pip install transformers==4.27.4
 pip install lightning==2.0.0
 pip install -i https://pypi.org/simple jsonargparse[signatures]==4.20.0
+pip install matplotlib==3.7.0
+pip install openpyxl==3.1.1
+pip install SciencePlots==2.0.0
+pip install -i https://pypi.org/simple stanza==1.5.0
+pip install -i https://pypi.org/simple notebook==6.5.4
+pip install pandas==2.0.0
 pip install yapf
