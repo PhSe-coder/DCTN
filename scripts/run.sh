4385b78c39eb6235f537ea52fa3520f11a154a29
PhSe-coder <2368732089@qq.com>
2023-06-06 20:25:53 +0800

    scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python config/update.py --source service --target restaurant
-python trainers.py fit --config config/pretrain.yaml
-python trainers.py fit --config config/train.yaml
+# python config/update.py --source service --target restaurant --max_epochs 30 --dim 300
+# python trainers.py fit --config config/pretrain.yaml
+# python trainers.py fit --config config/train.yaml
 model_path=/root/autodl-tmp/lightning_logs/*/checkpoints/fdgr-*.ckpt
 declare -a paths=($(ls -v $model_path)) # list all the model checkpoints
 python trainers.py test --config config/test.yaml --ckpt_path ${paths[-1]}
