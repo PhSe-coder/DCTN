706a9b32f6a603f027c4df9501d11325bd6ebd37
PhSe-coder <2368732089@qq.com>
2023-06-08 18:52:12 +0800

    scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# python config/update.py --source service --target restaurant --max_epochs 30 --dim 300
-# python trainers.py fit --config config/pretrain.yaml
-# python trainers.py fit --config config/train.yaml
-model_path=/root/autodl-tmp/lightning_logs/*/checkpoints/fdgr-*.ckpt
-declare -a paths=($(ls -v $model_path)) # list all the model checkpoints
-python trainers.py test --config config/test.yaml --ckpt_path ${paths[-1]}
+export TRANSFORMERS_OFFLINE=1
+python config/update.py --source service --target restaurant --max_pretrain_epochs 15 --max_train_epochs 15 --dim 300 --p 1.0
+# python trainers.py fit --config config/pretrain-source.yaml
+python trainers.py fit --config config/pretrain-target.yaml
+for coff in 0.01 0.02 0.03 0.04 0.05
+do
+    python trainers.py fit --config config/train.yaml --model.init_args.coff $coff
+    model_path=/root/autodl-tmp/lightning_logs/*/checkpoints/fdgr-*.ckpt
+    declare -a paths=($(ls -v $model_path)) # list all the model checkpoints
+    python trainers.py test --config config/test.yaml --ckpt_path ${paths[-1]}
+done
