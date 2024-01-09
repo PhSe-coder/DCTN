b0ea639ba611944a87e8d67c0279812fbdea5ac4
Phse-Coder <2368732089@qq.com>
2024-01-09 16:38:58 +0800

    scripts

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd)
-# export HTTP_PROXY=http://127.0.0.1:7890
-# export HTTPS_PROXY=http://127.0.0.1:7890
-python ./preprocess/extract.py --data-dir ./data/raw --threshold 0.3 --dest ./data
+export HTTP_PROXY=http://127.0.0.1:7890
+export HTTPS_PROXY=http://127.0.0.1:7890
 python ./preprocess/preprocess.py
+python ./preprocess/extract.py --data-dir ./data/raw --threshold 0.3 --dest ./data
 python ./preprocess/split.py --src ./data/dataset --dest ./processed/tmp --seed 42 --ratio 0.8
 python ./preprocess/ann.py
