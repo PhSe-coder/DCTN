5476c250c2be1ba569b7d02c9ab15e310ed1202d
Phse-Coder <2368732089@qq.com>
2024-01-10 15:06:38 +0800

    scripts

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-domains=('restaurant' 'service' 'laptop' 'device')
-
-export CUDA_VISIBLE_DEVICES=0
+domains=('restaurant' 'laptop' 'twitter')
 export TRANSFORMERS_OFFLINE=1
 for tar_domain in ${domains[@]};
 do
@@ -9,24 +7,8 @@ do
     do
         if [ $src_domain != $tar_domain ];
         then
-            if [ $src_domain == 'laptop' -a  $tar_domain == 'device' ];
-            then
-                continue
-            fi
-            if [ $src_domain == 'device' -a  $tar_domain == 'laptop' ];
-            then
-                continue
-            fi
-            echo "Update config yaml files"
-            python config/update.py --source $src_domain --target $tar_domain
-            echo "Pretrain: $src_domain -> $tar_domain"
-	        python trainers.py fit --config config/pretrain.yaml
             echo "Train: $src_domain -> $tar_domain"
-            python trainers.py fit --config config/train.yaml
-            model_path=/root/autodl-tmp/lightning_logs/*/checkpoints/fdgr-*.ckpt
-            declare -a paths=($(ls -v $model_path)) # list all the model checkpoints
-            echo "Test: $src_domain -> $tar_domain"
-            python trainers.py test --config config/test.yaml --ckpt_path ${paths[-1]}
+            python trainers.py --config config/train.yaml
         fi
     done
 done
--- scripts/tune.sh
+++ scripts/tune.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 export TRANSFORMERS_OFFLINE=1
-export HF_DATASETS_OFFLINE=1
-
-# python trainers.py fit --config config/pretrain-source.yaml
-
-for p in 0 # 1.0 0.8 0.6 0.4 0.2
+for h_dim in 100 200 300
 do
-    python config/update.py --source laptop --target restaurant --max_pretrain_epochs 15 --max_train_epochs 15 --dim 300 --p $p
-    python trainers.py fit --config config/pretrain-target.yaml --trainer.enable_progress_bar false
-    for coff in 0.005 0.01 0.02 0.03 0.04 0.05 0.06
+    for epoch in 5 10 15 20 25
     do
-        for coff_mi in 0.005 0.01 0.015 0.02
-        do
-            python trainers.py fit --config config/train.yaml --model.init_args.coff $coff --model.init_args.coff_mi $coff_mi --trainer.enable_progress_bar false
-            model_path=/root/autodl-tmp/lightning_logs/*/checkpoints/fdgr-*.ckpt
-            declare -a paths=($(ls -v $model_path)) # list all the model checkpoints
-            python trainers.py test --config config/test.yaml --ckpt_path ${paths[-1]} --trainer.enable_progress_bar false
-            echo "p: $p, coff: $coff"
-        done
+        echo "h_dim: $h_dim, epoch: $epoch"
+        python trainers.py --config config/train.yaml \
+                           --model.init_args.h_dim $h_dim \
+                           --trainer.enable_progress_bar false \
+                           --trainer.max_epochs $epoch
     done
 done
